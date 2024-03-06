84387f50c8c0e566f5d1d03bf8271fc6fc31c277
deityzs <kongkonghihi@gmail.com>
2024-03-07 02:55:07 +0700

    Merging home and dasboard pages

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const PORT = 8000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html')
+});
+
+app.get('/dashboard', (req, res) => {
     res.sendFile(__dirname + '/views/dashboard.html')
 });
+
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
